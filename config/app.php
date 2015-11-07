46f499a7285eb4a30f2a84ed4de54ef2847ceda1
usama ahmed <uusa35@gmail.com>
2015-11-07 16:27:10 +0300

    change the chiper generate key to AES-128-CBC

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
