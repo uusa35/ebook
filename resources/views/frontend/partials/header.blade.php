b7128e396775510c6604745a986279b4b43f2652
usama ahmed <uusa35@gmail.com>
2015-11-15 17:15:52 +0300

    adjusting header

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -2,15 +2,16 @@
     <div class="col-lg-8 col-lg-offset-2">
         <div class="page-header">
             <div class="row">
-                <div class="col-lg-9">
+                <div class="col-lg-2" style="float:right; text-align: right;">
+                    <img class=" img-responsive img-circle"
+                         src="{!! asset('images/logo.png') !!}" alt="" style="max-width:100px;"/>
+                </div>
+                <div class="col-lg-9" style="float: left; text-align: left;">
                     <h1>{{ trans('general.ebook') }}
                         <small>| e-boook.com</small>
                     </h1>
                 </div>
-                <div class="col-lg-2">
-                    <img class="{{ Session::get('pullClassReverse') }} img-responsive img-circle"
-                         src="{!! asset('images/logo.png') !!}" alt="" style="max-width:100px;"/>
-                </div>
+
             </div>
         </div>
         <div class="row">
