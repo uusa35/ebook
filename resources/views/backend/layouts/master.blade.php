c7454becd9735784d5d3d5ca9f22449795579820
usama ahmed <uusa35@gmail.com>
2015-11-17 11:41:19 +0300

    fixing menu issue

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -7,6 +7,7 @@
         @include('styles.stylesheets')
     @show
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
