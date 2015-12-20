7e16a4cc838ab4354bb4d5ceb5a4473b0a0de748
usama ahmed <uusa35@gmail.com>
2015-12-20 10:23:18 +0300

    fixing small bug for translations

--- resources/views/backend/partials/buttons/_chapter_change_status_btn.blade.php
+++ resources/views/backend/partials/buttons/_chapter_change_status_btn.blade.php
@@ -4,14 +4,14 @@
     @if($chapter->status == 'pending')
         <a class="{!! Config::get('button.btn-drafted') !!}"
            href="{{ action('Backend\ChaptersController@getUpdateChapterStatus',[$chapter->id,'drafted']) }}"
-           title="{{ trans('general.title.go_drafted') }}">
+           title="{{ trans('general.go_drafted') }}">
             {!! Config::get('button.icon-drafted') !!}
         </a>
         {{-- if the user just submitted to admin for approval --}}
    {{-- @elseif($chapter->status == 'drafted')
         <a class="{!! Config::get('button.btn-published') !!}"
            href="#" disabled
-           title="{{ trans('general.title.waiting_for_admin') }}">
+           title="{{ trans('general.waiting_for_admin') }}">
             {!! Config::get('button.icon-published') !!}
         </a>
     @endif--}}
@@ -21,27 +21,27 @@
     @if($chapter->status == 'pending')
         <a class="{!! Config::get('button.btn-drafted') !!}"
            href="{{ action('Backend\ChaptersController@getUpdateChapterStatus',[$chapter->id,'drafted']) }}"
-           title="{{ trans('general.title.go_drafted') }}">
+           title="{{ trans('general.go_drafted') }}">
             {!! Config::get('button.icon-drafted') !!}
         </a>
         {{-- if the user just submitted to admin for approval --}}
     @elseif($chapter->status == 'drafted')
         <a class="{!! Config::get('button.btn-published') !!}"
            href="{{ action('Backend\ChaptersController@getUpdateChapterStatus',[$chapter->id,'published']) }}"
-           title="{{ trans('general.title.go_published') }}">
+           title="{{ trans('general.go_published') }}">
             {!! Config::get('button.icon-published') !!}
         </a>
     @elseif($chapter->status == 'published')
         <a class="{!! Config::get('button.btn-declined') !!}"
            href="{{ action('Backend\ChaptersController@getUpdateChapterStatus',[$chapter->id,'declined']) }}"
-           title="{{ trans('general.title.go_pending') }}">
+           title="{{ trans('general.go_pending') }}">
             {!! Config::get('button.icon-pending') !!}
         </a>
         {{-- if the admin approved the book --}}
     @elseif($chapter->status == 'declined')
         <a class="{!! Config::get('button.btn-pending') !!}"
            href="{{ action('Backend\ChaptersController@getUpdateChapterStatus',[$chapter->id,'pending']) }}"
-           title="{{ trans('general.title.pending') }}">
+           title="{{ trans('general.pending') }}">
             {!! Config::get('button.icon-pending') !!}
         </a>
     @endif
