2a76914b393cb57efecbe8f3b8ce522ee148b6bc
usama ahmed <uusa35@gmail.com>
2015-12-26 17:21:53 +0300

    finializing the comment pack

---
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ChaptersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory('App\Src\Book\Chapter\Chapter',15)->create();
+    }
+}
