fd4eb6c4e116b9faad3098008eca56ab0974a774
usama ahmed <uusa35@gmail.com>
2016-01-08 19:41:58 +0300

    fixing a small bug within the index - getting the recentest books ordered by the first chapter that was published

--- resources/views/frontend/modules/book/all.blade.php
+++ resources/views/frontend/modules/book/all.blade.php
@@ -10,51 +10,53 @@
             <hr>
         </div>
         @if(count($allBooks) > 0)
-            @foreach($allBooks as $book)
-                <div class="col-lg-3 col-md-3 col-xs-10 col-md-offset-0 col-lg-offset-0 col-xs-offset-1">
+            <div class="row">
+                @foreach($allBooks as $book)
+                    <div class="col-lg-3 col-md-3 col-xs-10 col-md-offset-0 col-lg-offset-0 col-xs-offset-1">
 
-                    <div class=" box box-widget widget-user shadow-z-1">
-                        <!-- Add the bg color to the header using any of the bg-* classes -->
-                        <a class="" href="{{ action('BookController@show',$book->id) }}">
-                            <div class="widget-user-header bg-white"
-                                 style="background: url('{{ asset('images/uploads/cover/thumbnail/'.$book->cover) }}') center center; background-size: cover;">
-                            </div>
-                        </a>
+                        <div class=" box box-widget widget-user shadow-z-1">
+                            <!-- Add the bg color to the header using any of the bg-* classes -->
+                            <a class="" href="{{ action('BookController@show',$book->id) }}">
+                                <div class="widget-user-header bg-white"
+                                     style="background: url('{{ asset('images/uploads/cover/thumbnail/'.$book->cover) }}') center center; background-size: cover;">
+                                </div>
+                            </a>
 
-                        <div class="box-footer">
-                            <div class="row">
-                                <div class="col-sm-4 col-xs-4 border-right">
-                                    <div class="description-block">
-                                        <h3 class="description-header"><i class="fa fa-fw fa-heart text-danger"></i>
-                                        </h3>
-                                        <span class="description-text">{{ count($book->usersFavorites) }}</span>
+                            <div class="box-footer">
+                                <div class="row">
+                                    <div class="col-sm-4 col-xs-4 border-right">
+                                        <div class="description-block">
+                                            <h3 class="description-header"><i class="fa fa-fw fa-heart text-danger"></i>
+                                            </h3>
+                                            <span class="description-text">{{ count($book->usersFavorites) }}</span>
+                                        </div>
+                                        <!-- /.description-block -->
                                     </div>
-                                    <!-- /.description-block -->
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-sm-4 col-xs-4 border-right">
-                                    <div class="description-block">
-                                        <h5 class="description-header"><i class="fa fa-fw fa-calendar text-success"></i>
-                                        </h5>
-                                        <span class="description-text">{{ $book->chapters->first()->published_at->toDateString()}}</span>
+                                    <!-- /.col -->
+                                    <div class="col-sm-4 col-xs-4 border-right">
+                                        <div class="description-block">
+                                            <h5 class="description-header"><i
+                                                        class="fa fa-fw fa-calendar text-success"></i>
+                                            </h5>
+                                            <span class="description-text">{{ $book->chapters->first()->published_at->toDateString()}}</span>
+                                        </div>
+                                        <!-- /.description-block -->
                                     </div>
-                                    <!-- /.description-block -->
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-sm-4 col-xs-4">
-                                    <div class="description-block">
-                                        <h5 class="description-header"><i class="fa fa-fw fa-eye text-info"></i>
-                                        </h5>
-                                        <span class="description-text">{{ $book->views }}</span>
+                                    <!-- /.col -->
+                                    <div class="col-sm-4 col-xs-4">
+                                        <div class="description-block">
+                                            <h5 class="description-header"><i class="fa fa-fw fa-eye text-info"></i>
+                                            </h5>
+                                            <span class="description-text">{{ $book->views }}</span>
+                                        </div>
+                                        <!-- /.description-block -->
                                     </div>
-                                    <!-- /.description-block -->
+                                    <!-- /.col -->
                                 </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <a class="" href="{{ action('BookController@show',$book->id) }}">
+                                <!-- /.row -->
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <a class="" href="{{ action('BookController@show',$book->id) }}">
                                 <span class="description-text">
                                     <h5>
                                         {{ \Illuminate\Support\Str::words($book->title,4) }}
@@ -62,14 +64,15 @@
 
                                     {{ \Illuminate\Support\Str::words($book->description,3) }}
                             </span>
-                                    </a>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- /.widget-user -->
                     </div>
-                    <!-- /.widget-user -->
-                </div>
-            @endforeach
+                @endforeach
+            </div>
             <div class="row">
                 <div class="col-lg-4 col-xs-6 col-xs-3 col-lg-offset-4 text-center">
                     {!! $allBooks->render() !!}
