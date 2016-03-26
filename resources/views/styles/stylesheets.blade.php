af7d71b2b11f2e251634aad9f947900f9f107cec
usama ahmed <uusa35@gmail.com>
2016-03-26 21:08:08 +0300

    change in the stylesheets

--- resources/views/styles/stylesheets.blade.php
+++ resources/views/styles/stylesheets.blade.php
@@ -4,6 +4,11 @@
 <link rel="stylesheet" href="/css/backend.css"/>
 <link rel="stylesheet" href="/css/material-design.css"/>
 <link rel="stylesheet" href="/css/abstract-css.css"/>
+<style type="text/css">
+    .widget-user .widget-user-header {
+        min-height: 350px !important;
+    }
+</style>
 @if(App::getLocale() === 'ar')
     <link href="/css/bootstrap-rtl.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/bootstrap-flipped.min.css"/>
